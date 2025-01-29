e39f0f92c8976e203c597d0e078c6a38bcf39cf2
Nick Morin <morinnick31@gmail.com>
2025-01-29 13:34:47 +0300

    add english lang files

---
+++ components/myComponents/date.current/lang/en/.description.php
@@ -0,0 +1,5 @@
+<?php
+$MESS ['MAIN_CURDATE_NAME'] = "Current date";
+$MESS ['MAIN_CURDATE_DESCRIPTION'] = "Display the current date";
+$MESS ['CHILD_CURDATE_NAME'] = "Custom components";
+$MESS ['COMPONENTS_DIR_NAME'] = "My components";
---
+++ components/myComponents/weather.info/lang/en/.description.php
@@ -0,0 +1,5 @@
+<?php
+$MESS ['MAIN_WEATHER_NAME'] = "Weather forecast";
+$MESS ['MAIN_WEATHER_DESCRIPTION'] = "Display the weather forecast";
+$MESS ['CHILD_WEATHER_NAME'] = "Weather";
+$MESS ['COMPONENTS_DIR_NAME'] = "My components";
